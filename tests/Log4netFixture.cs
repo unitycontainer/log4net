05b28268c9fea0dd944a3ca2d26c8821233b5f95
Eugene Sadovoi <evgeni@eniks.com>
2018-01-10 23:58:19 -0500

    Added projects

---
+++ tests/Log4netFixture.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace log4net.Tests
+{
+    [TestClass]
+    public class Log4netFixture
+    {
+        [TestMethod]
+        public void AddExtension()
+        {
+        }
+    }
+}
